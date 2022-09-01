b9dcd047709993a3f94a69808fe8da4388377c5c
carmtsang <carmen.tsang1208@hotmail.com>
2022-09-01 16:15:45 -0700

    add validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Input a store name:"
+
+user_store = gets.chomp
+
+new_store = Store.create(name: user_store)
+puts new_store.errors.full_messages
